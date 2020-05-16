6afd6efd6b6244a1e8fb5c2d240b0009e77c2c5d
Mark Boleigha <donhoenix@gmail.com>
2020-05-16 17:28:25 +0100

    initial setup

---
+++ SwiftyNav/SwiftyNav.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyNav.h
+//  SwiftyNav
+//
+//  Created by Sprinthub on 16/05/2020.
+//  Copyright © 2020 Sprinthub Mobile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyNav.
+FOUNDATION_EXPORT double SwiftyNavVersionNumber;
+
+//! Project version string for SwiftyNav.
+FOUNDATION_EXPORT const unsigned char SwiftyNavVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyNav/PublicHeader.h>
+
+
